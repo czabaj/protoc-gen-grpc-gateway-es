5731ed44a854e8f38d191142dca5caae0c262870
Václav Gröhling <grohling.v@clav.cz>
2023-11-15 12:54:01 +0100

    chore: improve publish automation

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,6 +1,26 @@
 const version = "${version}";
 
 module.exports = {
+  git: {
+    push: true,
+    tagName: `v${version}`,
+    commitsPath: ".",
+    commitMessage: `chore: released version v${version}`,
+    requireCommits: true,
+    requireCommitsFail: false,
+  },
+  github: {
+    release: true,
+    releaseName: `v${version}`,
+  },
+  hooks: {
+    "before:init": "bun test",
+    "before:git:release": ["git add --all"],
+  },
+  npm: {
+    publish: false,
+    versionArgs: ["--workspaces false"],
+  },
   plugins: {
     "@release-it/conventional-changelog": {
       path: ".",
@@ -10,24 +30,5 @@ module.exports = {
         path: ".",
       },
     },
-  },
-  git: {
-    push: true,
-    tagName: `v${version}`,
-    commitsPath: ".",
-    commitMessage: `chore: released version v${version}`,
-    requireCommits: true,
-    requireCommitsFail: false,
-  },
-  npm: {
-    publish: false,
-    versionArgs: ["--workspaces false"],
-  },
-  github: {
-    release: true,
-    releaseName: `v${version}`,
-  },
-  hooks: {
-    "before:git:release": ["git add --all"],
   },
 };
